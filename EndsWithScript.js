caeb0f8352eedc90dbe462460f735b08e09e4e5d
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-15 13:53:02 +0530

    Feat: Add function check a word ends with 'Script'.

--- EndsWithScript.js
+++ EndsWithScript.js
@@ -2,5 +2,10 @@ function endsWithScript(word){
     return word.endsWith("Script")
 }
 
+endsWithScriptArrow = (word) => {
+    return word.endsWith("Script")
+}
+
 word = "javascript"
 console.log(`By regular function: ${endsWithScript(word)}`)
+console.log(`By arrow function: ${endsWithScriptArrow(word)}`)
