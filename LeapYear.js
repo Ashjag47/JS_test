8a2515040b207a42977770231bfd519b4a625659
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-15 13:41:32 +0530

    Feat: Add function check year is leap or not

---
+++ LeapYear.js
@@ -0,0 +1,11 @@
+function isLeapYear(year) {
+    return ((year%4==0) && (year%100!=0) || (year%400==0)) ? true : false
+}
+
+isLeapYearArrow = (year) => {
+    return ((year%4==0) && (year%100!=0) || (year%400==0)) ? true : false
+}
+
+year=2001
+console.log(`By regular function: ${isLeapYear(year)}`)
+console.log(`By Arrow function: ${isLeapYearArrow(year)}`)
