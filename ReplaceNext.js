96ec3b92baa7e312ea3789f3d6ee9ff952032b31
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-15 18:47:36 +0530

    Feat: Add function to replace char by next char in word

---
+++ ReplaceNext.js
@@ -0,0 +1,38 @@
+function replaceNext(word){
+    let wordArr = word.split("")
+    for(let i=0; i<wordArr.length; i++){
+        if(wordArr[i]>='A' && wordArr[i]<='Z'){
+            let n = wordArr[i].charCodeAt()-'A'.charCodeAt()
+            n=(n+1)%26
+            wordArr[i]=String.fromCharCode(n+'A'.charCodeAt())
+        }
+        else if(wordArr[i]>='a' && wordArr[i]<='z'){
+            let n = wordArr[i].charCodeAt()-'a'.charCodeAt()
+            n=(n+1)%26
+            wordArr[i]=String.fromCharCode(n+'a'.charCodeAt())
+        }
+    }
+    return wordArr.join("")
+}
+
+replaceNextArrow = (word) => {
+    let wordArr = word.split("")
+    for(let i=0; i<wordArr.length; i++){
+        if(wordArr[i]>='A' && wordArr[i]<='Z'){
+            let n = wordArr[i].charCodeAt()-'A'.charCodeAt()
+            n=(n+1)%26
+            wordArr[i]=String.fromCharCode(n+'A'.charCodeAt())
+        }
+        else if(wordArr[i]>='a' && wordArr[i]<='z'){
+            let n = wordArr[i].charCodeAt()-'a'.charCodeAt()
+            n=(n+1)%26
+            wordArr[i]=String.fromCharCode(n+'a'.charCodeAt())
+        }
+    }
+    return wordArr.join("")   
+}
+
+
+let word="Codeacademy"
+console.log(`By regular function: ${replaceNext(word)}`)
+console.log(`By Arrow function: ${replaceNextArrow(word)}`)
