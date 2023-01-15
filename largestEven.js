57487c5c9926a27d620e27f9cfbc4e1765ccdbbd
Ashjag47 <Ashish_P@mckinsey.com>
2023-01-15 18:22:21 +0530

    Feat: Add function to find largest even in array

--- largestEven.js
+++ largestEven.js
@@ -1,21 +1,27 @@
 //regular function
 function largestEven(arr){
-    let big=-1
+    let big=Number.NEGATIVE_INFINITY
     for(let i in arr){
         if(arr[i]%2==0 && arr[i]>big){
             big=arr[i]
         }
+    }
+    if(big==Number.NEGATIVE_INFINITY){
+        return -1
     }
     return big
 }
 
 //arrow function
 largestEvenArrow = () => {
-    let big=-1
+    let big=Number.NEGATIVE_INFINITY
     for(let i in arr){
         if(arr[i]%2==0 && arr[i]>big){
             big=arr[i]
         }
+    }
+    if(big==Number.NEGATIVE_INFINITY){
+        return -1
     }
     return big   
 }
